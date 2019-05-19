4594494a108b9527a54bc60bd51b8b59453da80f
Becky <30902005+beckybanos@users.noreply.github.com>
2019-05-19 16:08:17 -0500

    Update shooting2.m

--- Tercer parcial/Solución Examen Tercer Parcial/shooting2.m
+++ Tercer parcial/Solución Examen Tercer Parcial/shooting2.m
@@ -1,7 +1,8 @@
+%Metodo de shooting2:cambian los valores de as ys de entrada. Utilizas el valor
+%inicial del valor y y z. No sabemos canto es y1, pero sabemos el valor final de z,
+%y lo comparamos con el valor de z1
 function dif = shooting2(f,t0,tf,y11,y21,h,y22)
     y0 = [y11; y21];
     [t, y] = ivps(f,t0,tf,y0,h, 4);
     dif = y22 - y(2,end);
 end
-
-
