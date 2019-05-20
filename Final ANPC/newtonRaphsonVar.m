76dbb650a6db9e3c4e9ff5332bd223ff1e35b72a
hca <hca@itam.mx>
2019-05-20 18:37:57 -0500

    Examen Final ANPC

---
+++ Final ANPC/newtonRaphsonVar.m
@@ -0,0 +1,15 @@
+%Función de Newton Raphson con muchas variables
+function [x,i] = newtonRaphsonVar(f,J,x)
+    TOL = eps;
+    MAX = 66;    
+    
+    i = 0;
+    cond = true;
+    while cond
+        xp = x;
+        x = xp - J(xp)\f(xp);
+        i = i + 1;
+        cond = (norm(x-xp)/norm(x)) > TOL && all(f(x) ~=0) && i <= MAX; %cuando no se cumple ninguna de esta se cambia a false
+    end
+end
+
